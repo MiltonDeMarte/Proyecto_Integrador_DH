3f5876ed3a5d37b92b5c1524b0b147c0706f4478
milto <miltondemarte@gmail.com>
2021-11-04 17:35:39 -0300

    Primer upload proyecto

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+
+const path = require("path")
+const publicDomain = path.resolve(__dirname, "./public")
+
+
+app.listen(3000,() => {
+    console.log("Servidor corriendo en el puerto 3000")
+})
+
+
+app.use(express.static(publicDomain))
+
+
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/index.html"))
+})
+
+
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/register.html"))
+})
+
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/login.html"))
+})
