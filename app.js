10d719aef5fe6bc958cafd83ae2cd00165a7f9af
milto <miltondemarte@gmail.com>
2021-11-04 19:54:33 -0300

    agregando Procfile y modificando app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path")
 const publicDomain = path.resolve(__dirname, "./public")
 
 
-app.listen(3000,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log("Servidor corriendo en el puerto 3000")
 })
 
